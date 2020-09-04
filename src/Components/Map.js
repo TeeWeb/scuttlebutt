7547fac691e0bb0dfa0c40bf95d27fd6a0b4aa78
teeweb <tylerweber@live.com>
2020-09-03 22:39:43 -0700

    Refactored Map component to stateless function component

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -4,44 +4,31 @@ import "./Map.css";
 import "./Player";
 import Player from "./Player";
 
-export default class Map extends React.Component {
-  state = {
-    currentPlayer: this.props.currentPlayer,
-    players: this.props.players
-  };
-
-  handleClick = event => {
+export default function Map(props) {
+  function handleClick(event) {
     console.log("Map click location:", event.clientX, event.clientY);
-  };
-
-  componentDidMount() {
-    this.setState({
-      currentPlayer: this.props.currentPlayer,
-      players: this.props.players
-    });
   }
 
-  render() {
-    const activePlayers = this.state.players.map((player, i) => {
-      if (player.isAlive) {
-        return (
-          <Player
-            key={i}
-            id={player.id}
-            name={player.name}
-            playerColor={player.playerColor}
-            isPlayersTurn={player.isPlayersTurn}
-            units={player.units}
-          />
-        );
-      } else {
-        return false;
-      }
-    });
-    return (
-      <div id="map" className="map" onClick={this.handleClick}>
-        {activePlayers}
-      </div>
-    );
-  }
+  const activePlayers = props.players.map((player, i) => {
+    if (player.isAlive) {
+      return (
+        <Player
+          key={i}
+          id={player.id}
+          name={player.name}
+          playerColor={player.playerColor}
+          isPlayersTurn={player.id === props.currentPlayer.id ? true : false}
+          units={player.units}
+        />
+      );
+    } else {
+      return false;
+    }
+  });
+
+  return (
+    <div id="map" className="map" onClick={handleClick}>
+      {activePlayers}
+    </div>
+  );
 }
